a2095c81b3be3d27916d336c3e9e6c93927b0853
dev-yakuza <dev.yakuza@gmail.com>
2020-02-17 17:23:40 +0900

    add change ruby version

--- rails/install.sh
+++ rails/install.sh
@@ -1,5 +1,9 @@
 # !/bin/bash
 
-rbenv install $(rbenv install -l | sed -n '/^[[:space:]]*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}[[:space:]]*$/ h;${g;p;}')
+VERSION = $(rbenv install -l | sed -n '/^[[:space:]]*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}[[:space:]]*$/ h;${g;p;}')
+
+rbenv install ${VERSION}
 
 rbenv rehash
+
+rbenv global ${VERSION}
