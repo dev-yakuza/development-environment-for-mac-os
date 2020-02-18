2f144fe26ccf6b21bacd013f14ddf690f00f2941
dev-yakuza <dev.yakuza@gmail.com>
2020-02-18 17:58:37 +0900

    modify rails

--- rails/install.sh
+++ rails/install.sh
@@ -3,9 +3,13 @@
 VERSION = $(rbenv install -l | sed -n '/^[[:space:]]*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}[[:space:]]*$/ h;${g;p;}')
 
 rbenv install ${VERSION}
-
 rbenv rehash
 
 rbenv global ${VERSION}
+rbenv rehash
 
-gem install bundler rails
+gem install bundler
+rbenv rehash
+
+gem install rails
+rbenv rehash
