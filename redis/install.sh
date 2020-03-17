8995858e1b52b192635999712c6c319341be5f52
dev-yakuza <dev.yakuza@gmail.com>
2020-03-17 16:48:15 +0900

    add stop redis command

--- redis/install.sh
+++ redis/install.sh
@@ -1,6 +1,9 @@
 ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
 
+# stop redis
+# launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
+
 # remove redis
 # brew uninstall redis
 # rm ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
