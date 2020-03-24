163595b2be64ec0c29fa88666e5b70b3a9c1331e
dev-yakuza <dev.yakuza@gmail.com>
2020-03-24 09:48:03 +0900

    change install brew path

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 # install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # install via brew
 brew bundle --file=./Brewfile
