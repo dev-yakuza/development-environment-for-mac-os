9287c8fb9b31bf0c00e66ce39e051682da4497b8
dev-yakuza <dev.yakuza@gmail.com>
2021-06-16 09:33:10 +0900

    Add terraform

--- vscode/install.sh
+++ vscode/install.sh
@@ -53,6 +53,7 @@ code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension wingrunr21.vscode-ruby
 code --install-extension dart-code.flutter
 code --install-extension sleistner.vscode-fileutils
+code --install-extension hashicorp.terraform
 
 # copy vscode settings
 cp ./vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
