1670935204e2c830698efd4528388321c37fb6d1
dev-yakuza <dev.yakuza@gmail.com>
2021-07-18 15:09:29 +0900

    Remove unused

--- node/install.sh
+++ node/install.sh
@@ -7,8 +7,5 @@ source $(brew --prefix nvm)/nvm.sh
 nvm install v12.16.1
 brew install yarn
 
-# fix version
-npm config set save-exact=true
-
 # install git open
 npm install --global git-open
--- react-native/install.sh
+++ react-native/install.sh
@@ -1,8 +1,5 @@
 # !/bin/bash
 
 npm install -g react-native-cli
-npm install -g expo-cli
-
-npm config set save-exact=true
 
 sudo gem install cocoapods
