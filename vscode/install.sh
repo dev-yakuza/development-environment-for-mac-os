24ada75c054409186d976b2f537ed92d90eae5d7
dev-yakuza <dev.yakuza@gmail.com>
2022-10-06 16:26:59 +0900

    add live server extension

--- vscode/install.sh
+++ vscode/install.sh
@@ -59,6 +59,7 @@ code --install-extension johnsoncodehk.volar
 code --install-extension mrmlnc.vscode-remark
 code --install-extension flutterando.flutter-coverage
 code --install-extension unifiedjs.vscode-mdx
+code --install-extension ritwickdey.LiveServer
 
 # copy vscode settings
 cp ./vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
