e4ca21eae72f7ecba3b65b4469de5a8c89ee7cf9
dev-yakuza <dev.yakuza@gmail.com>
2023-05-09 15:29:57 +0900

    Remove and change deprecated extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -9,12 +9,10 @@ code --install-extension dzannotti.vscode-babel-coloring
 code --install-extension esbenp.prettier-vscode
 code --install-extension fterrag.vscode-php-cs-fixer
 code --install-extension gencer.html-slim-scss-css-class-completion
-code --install-extension jcbuisson.vue
+code --install-extension Vue.volar
 code --install-extension leizongmin.node-module-intellisense
 code --install-extension mf.vscode-styled-components
 code --install-extension ms-python.python
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
-code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension redhat.vscode-yaml
 code --install-extension RoscoP.ActiveFileInStatusBar
 code --install-extension shardulm94.trailing-spaces
@@ -47,7 +45,7 @@ code --install-extension hashicorp.terraform
 code --install-extension octref.vetur
 code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension arjun.swagger-viewer
-code --install-extension mrmlnc.vscode-remark
+code --install-extension unifiedjs.vscode-remark
 code --install-extension flutterando.flutter-coverage
 code --install-extension unifiedjs.vscode-mdx
 code --install-extension ritwickdey.LiveServer
