72bd8e4b7d0784c1b2b89bf14620607d0dbd3784
dev-yakuza <dev.yakuza@gmail.com>
2023-09-08 20:04:19 +0900

    Fix

---
+++ dock/configuration.sh
@@ -0,0 +1,29 @@
+defaults write com.apple.dock show-recents -bool false
+
+defaults write com.apple.dock persistent-apps -array
+sleep 10
+killall Dock
+apps=(
+  "/System/Applications/Launchpad.app"
+  "/Applications/Google Chrome.app"
+  "/Applications/Firefox.app"
+  "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
+  "/Applications/Microsoft Edge.app"
+  "/Applications/iTerm.app"
+  "/Applications/Visual Studio Code.app"
+  "/Applications/DBeaver.app"
+  "/Applications/Slack.app"
+  "/Applications/KakaoTalk.app"
+  "/Applications/LINE.app"
+  "/Applications/MySQLWorkbench.app"
+  "/Applications/Figma.app"
+  "/Applications/Postman.app"
+  "/Applications/Cyberduck.app"
+  "/Applications/Karabiner-Elements.app"
+  "/System/Library/CoreServices/Applications/Screen Sharing.app"
+)
+for app in "${apps[@]}"; do
+    defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>${app}</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+done
+sleep 10
+killall Dock
--- go/install.sh
+++ go/install.sh
@@ -1,3 +1,3 @@
 mkdir -p ~/go/bin
 cd ~/go/bin && go install github.com/smartystreets/goconvey@latest
-cd ~/go/bin && go install -u github.com/cosmtrek/air@latest
+cd ~/go/bin && go install github.com/cosmtrek/air@latest
