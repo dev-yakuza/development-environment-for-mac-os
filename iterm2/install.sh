f64b8cad4cbd4aa84e0b1f32e5865c34bc150d8a
dev-yakuza <dev.yakuza@gmail.com>
2023-09-09 09:42:06 +0900

    Fix

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,3 +1,5 @@
 # !/bin/bash
 
+rm -rf ~/Library/Application\ Support/iTerm2/DynamicProfiles/
+mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles/
 cp ./iterm2/com.googlecode.iterm2.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles/
