b500d1b2c135253d3d0f088092c7923e57c60e82
jeonghean.kim <jeonghean.kim@jeongheankims-MacBook-Air.local>
2023-09-09 16:04:12 +0900

    fix

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,7 +1,10 @@
 # !/bin/bash
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/projects/iterm2/"
+mkdir -p ~/.config/iterm2/custom/
+cp ./iterm2/com.googlecode.iterm2.plist ~/.config/iterm2/custom/
+
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.config/iterm2/custom/"
 
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
